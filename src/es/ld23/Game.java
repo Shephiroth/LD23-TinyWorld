99643258e3c38931fcfe689bb30ddcd972de74fe
Jorge Barcelo <shephiroth@hotmail.com>
2012-04-21 07:12:20 +0200

    Little fix, now black screen instead of Exceptions.
    Removed not implemented exceptions (damm netbeans) and place the applet loader thing. Now it works for application and applet (for application need say where natives are).
    
    But still, black screen, nothing happens yet :)

--- src/es/ld23/Game.java
+++ src/es/ld23/Game.java
@@ -1,8 +1,12 @@
 package es.ld23;
 
+import static org.lwjgl.opengl.GL11.*;
+
 public class Game {
 
 	private static final boolean debug = true;
+	private int width;
+	private int height;
 
 	static void debug(String string) {
 		if (debug) {
@@ -11,21 +15,26 @@ public class Game {
 	}
 
 	public Game(int w, int h) {
+		width = w;
+		height = h;
 	}
 
 	public void initGL() {
-		throw new UnsupportedOperationException("Not yet implemented");
+		glViewport(0, 0, width, height);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0, width, height, 0, 1, -1);
+		glMatrixMode(GL_MODELVIEW);
 	}
 
 	public void loadResources() {
-		throw new UnsupportedOperationException("Not yet implemented");
 	}
 
 	public void render() {
-		throw new UnsupportedOperationException("Not yet implemented");
 	}
 
 	public void tick() {
-		throw new UnsupportedOperationException("Not yet implemented");
 	}
 }
